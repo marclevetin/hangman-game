45bd9b8831d637c1e35337a4dc38b3768061a298
marclevetin <marc@derbyhat.org>
2017-10-06 13:17:24 -0400

    fixed bug in win condition whereby HANGMAN was advancing even though a correct letter was chosen

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,7 @@ const words = ["dog", "cat", "mouse", "cheese"];
 const hangman = "HANGMAN"
 let correctWord = '';
 let guessedLetters = "";
-let remainingCountOfPlayerGuesses = 7;
+let remainingCountOfPlayerGuesses = 8;
 let incorrectGuesses = 0;
 let numberWins = 0;
 let numberLosses = 0;
@@ -34,19 +34,22 @@ function updateDOM() {
   document.getElementById("number-losses").innerHTML = numberLosses;
   document.getElementById("guessed-letters").innerHTML = guessedLetters;
   document.getElementById("current-word").innerHTML = obfuscatedWord.split("").join(" ");
+  document.getElementById("hangman").innerHTML = hangman.slice(0,incorrectGuesses);
 }
 
 document.onkeypress = function(key) {
   // tracks keys and letters selected by the user
-  guessedLetters += key.key;
-  processGuess();
+  let keypress = key.key.toLowerCase();
+  guessedLetters += keypress;
+  processGuess(keypress);
   updateDOM();
   // endGame();
   setTimeout(endGame,100)
 }
 
-function processGuess() {
-  // this cycles through the whole word in case there are duplicate letters
+function processGuess(letter) {
+  // this creates the word to be displayed.
+  // it cycles through the whole word in case there are duplicate letters
   // look for ways to refactor and process only the new letter later.
   obfuscatedWord = '';
   for (i = 0; i < correctWord.length; i++) {
@@ -55,6 +58,11 @@ function processGuess() {
     } else {
         obfuscatedWord += "_";
     }
+  }
+
+  // this increments the Hangman word to show a user how close they are to losing
+  if (!correctWord.includes(letter)) {
+    incorrectGuesses++;
   }
 }
 
@@ -72,11 +80,11 @@ function endGame() {
     alert("You win!  The word is " + obfuscatedWord + ".");
     numberWins++;
     resetGame();
-  } else if (obfuscatedWord.includes("_") && incorrectGuesses == 7) {
+  } else if (obfuscatedWord.includes("_") && incorrectGuesses == 8) {
     alert("You lose!  The word is " + correctWord + ".");
     numberLosses++;
     resetGame();
-  } else if (obfuscatedWord.includes("_") && incorrectGuesses != 7) {
-    incorrectGuesses++;
+  } else if (obfuscatedWord.includes("_") && incorrectGuesses != 8) {
+    // incorrectGuesses++;
   }
 }
